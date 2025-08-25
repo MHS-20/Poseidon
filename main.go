1a12e6f56f7b32b251d0bf7463f3da94ffc9b7db
MHS <muhamad.salman2001@gmail.com>
2025-08-25 18:59:33 +0200

    feat: added create container method

--- main.go
+++ main.go
@@ -8,10 +8,38 @@ import (
 	"github.com/MHS-20/poseidon/node"
 	"github.com/MHS-20/poseidon/task"
 	"github.com/MHS-20/poseidon/worker"
+	"github.com/docker/docker/client"
 
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
 )
+
+func createContainer() (*task.Docker, *task.DockerResult) {
+	c := task.Config{
+		Name:  "test-container-1",
+		Image: "postgres:13",
+		Env: []string{
+			"POSTGRES_USER=poseidon",
+			"POSTGRES_PASSWORD=poseidon",
+		},
+	}
+
+	dc, _ := client.NewClientWithOpts(client.FromEnv)
+	d := task.Docker{
+		Client: dc,
+		Config: c,
+	}
+
+	result := d.Run()
+	if result.Error != nil {
+		fmt.Printf("%v\n", result.Error)
+		return nil, nil
+	}
+
+	fmt.Printf(
+		"Container %s is running with config %v\n", result.ContainerId, c)
+	return &d, &result
+}
 
 func main() {
 	t := task.Task{
