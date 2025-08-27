7a78ce89bf7fda57a06b646da807467dcd76d471
MHS <muhamad.salman2001@gmail.com>
2025-08-27 13:07:42 +0200

    chore: added goprocinfo lib

--- worker/stats.go
+++ worker/stats.go
@@ -1,10 +1,23 @@
 package worker
 
+import (
+	"github.com/c9s/goprocinfo/linux"
+)
+
 type Stats struct {
 	MemStats  *linux.MemInfo
 	DiskStats *linux.Disk
 	CpuStats  *linux.CPUStat
 	LoadStats *linux.LoadAvg
+}
+
+func GetStats() *Stats {
+	return &Stats{
+		MemStats:  GetMemoryInfo(),
+		DiskStats: GetDiskInfo(),
+		CpuStats:  GetCpuStats(),
+		LoadStats: GetLoadAvg(),
+	}
 }
 
 func (s *Stats) MemTotalKb() uint64 {
