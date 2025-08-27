840a4aa294a3f73c6c3051f84cc9fbd9a4cb9ef1
MHS <muhamad.salman2001@gmail.com>
2025-08-27 13:08:39 +0200

    feat: added helper functions

--- worker/stats.go
+++ worker/stats.go
@@ -1,6 +1,8 @@
 package worker
 
 import (
+	"log"
+
 	"github.com/c9s/goprocinfo/linux"
 )
 
@@ -57,4 +59,40 @@ func (s *Stats) CpuUsage() float64 {
 		return 0.00
 	}
 	return (float64(total) - float64(idle)) / float64(total)
+}
+
+func GetMemoryInfo() *linux.MemInfo {
+	memstats, err := linux.ReadMemInfo("/proc/meminfo")
+	if err != nil {
+		log.Printf("Error reading from /proc/meminfo")
+		return &linux.MemInfo{}
+	}
+	return memstats
+}
+
+func GetDiskInfo() *linux.Disk {
+	diskstats, err := linux.ReadDisk("/")
+	if err != nil {
+		log.Printf("Error reading from /")
+		return &linux.Disk{}
+	}
+	return diskstats
+}
+
+func GetCpuStats() *linux.CPUStat {
+	stats, err := linux.ReadStat("/proc/stat")
+	if err != nil {
+		log.Printf("Error reading from /proc/stat")
+		return &linux.CPUStat{}
+	}
+	return &stats.CPUStatAll
+}
+
+func GetLoadAvg() *linux.LoadAvg {
+	loadavg, err := linux.ReadLoadAvg("/proc/loadavg")
+	if err != nil {
+		log.Printf("Error reading from /proc/loadavg")
+		return &linux.LoadAvg{}
+	}
+	return loadavg
 }
