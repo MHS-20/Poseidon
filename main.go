20e31da4c4d6af8a8e00b15c641557dcba2bad0f
MHS <muhamad.salman2001@gmail.com>
2025-08-27 17:29:23 +0200

    test: updated main

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"os"
 	"strconv"
-	"time"
 
 	"github.com/MHS-20/poseidon/manager"
 	"github.com/MHS-20/poseidon/task"
@@ -55,54 +54,26 @@ func stopContainer(d *task.Docker, id string) *task.DockerResult {
 }
 
 func main() {
-	host := os.Getenv("POSEIDON_HOST")
-	port, _ := strconv.Atoi(os.Getenv("POSEIDON_PORT"))
+	whost := os.Getenv("POSEIDON_WORKER_HOST")
+	wport, _ := strconv.Atoi(os.Getenv("POSEIDON_WORKER_PORT"))
+	mhost := os.Getenv("POSEIDON_MANAGER_HOST")
+	mport, _ := strconv.Atoi(os.Getenv("POSEIDON_MANAGER_PORT"))
 
 	fmt.Println("Starting Poseidon worker")
-	fmt.Println(host)
-	fmt.Println(port)
-
 	w := worker.Worker{
 		Queue: *queue.New(),
 		Db:    make(map[uuid.UUID]*task.Task),
 	}
-	api := worker.Api{Address: host, Port: port, Worker: &w}
-
+	wapi := worker.Api{Address: whost, Port: wport, Worker: &w}
 	go w.RunTasks()
 	go w.CollectStats()
-	go api.Start()
+	go wapi.Start()
 
-	workers := []string{fmt.Sprintf("%s:%d", host, port)}
+	fmt.Println("Starting Poseidon manager")
+	workers := []string{fmt.Sprintf("%s:%d", whost, wport)}
 	m := manager.New(workers)
-
-	for i := 0; i < 3; i++ {
-		t := task.Task{
-			ID:    uuid.New(),
-			Name:  fmt.Sprintf("test-container-%d", i),
-			State: task.Scheduled,
-			Image: "strm/helloworld-http",
-		}
-		te := task.TaskEvent{
-			ID:    uuid.New(),
-			State: task.Running,
-			Task:  t,
-		}
-		m.AddTask(te)
-		m.SendWork()
-	}
-
-	func() {
-		for {
-			fmt.Printf("[Manager] Updating tasks from %d workers\n", len(m.Workers))
-			m.UpdateTasks()
-			time.Sleep(15 * time.Second)
-		}
-	}()
-
-	for {
-		for _, t := range m.TaskDb {
-			fmt.Printf("[Manager] Task: id: %s, state: %d\n", t.ID, t.State)
-			time.Sleep(15 * time.Second)
-		}
-	}
+	mapi := manager.Api{Address: mhost, Port: mport, Manager: m}
+	go m.ProcessTasks()
+	go m.UpdateTasks()
+	mapi.Start()
 }
