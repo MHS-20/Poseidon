8967774f19881a766cf39ca0f13282e03e375450
MHS <muhamad.salman2001@gmail.com>
2025-08-29 16:31:34 +0200

    feat: added in memory task store impl

--- store/store.go
+++ store/store.go
@@ -1,16 +1,5 @@
 package store
 
-import (
-	"github.com/MHS-20/poseidon/task"
-	"encoding/json"
-	"fmt"
-	"log"
-	"os"
-
-	"github.com/boltdb/bolt"
-)
-
-
 type Store interface {
  Put(key string, value interface{}) error
  Get(key string) (interface{}, error)
